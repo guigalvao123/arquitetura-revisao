1f2b4a9e623dadb20c53a65a43514a708be628ec
guigalvao123 <guilherme.galvaopadua@gmail.com>
2023-02-09 15:01:51 -0300

    Controler estabelecido

---
+++ src/controller/ProductController.ts
@@ -0,0 +1,92 @@
+import { Request, Response } from "express"
+import knex from "knex"
+
+
+export class ProductController {
+    constructor() { }
+
+    public getProducts = async (req: Request, res: Response) => {
+        try {
+            const q = req.query.q
+            const db = knex({
+                client: "sqlite3",
+                connection: {
+                    filename: "./src/database/arquitetura.db",
+                },
+                useNullAsDefault: true,
+                pool: {
+                    min: 0,
+                    max: 1,
+                    afterCreate: (conn: any, cb: any) => {
+                        conn.run("PRAGMA foreign_keys = ON", cb)
+                    }
+                }
+            })
+
+            if (q) {
+                const productsDB = await db("products")
+                .select()
+                .where("name","LIKE",`${q}`)
+
+                const brandsDB = await db("brands").select()
+
+                const products = productsDB.map((productDB) => {
+                    return {
+                        id: productDB.id,
+                        name: productDB.name,
+                        price: productDB.price,
+                        brand: getBrand(productDB.brand_id)
+                    }
+                })
+
+                function getBrand(brandId: string) {
+                    const brand = brandsDB.find((brandDB) => {
+                        return brandDB.id === brandId
+                    })
+
+                    return {
+                        brand: brand.id,
+                        name: brand.name
+                    }
+                }
+
+                res.status(200).send(products)
+            } else {
+                const productsDB = await db("products").select()
+
+                const brandsDB = await db("brands").select()
+
+                const products = productsDB.map((productDB) => {
+                    return {
+                        id: productDB.id,
+                        name: productDB.name,
+                        price: productDB.price,
+                        brand: getBrand(productDB.brand_id)
+                    }
+                })
+
+                function getBrand(brandId: string) {
+                    const brand = brandsDB.find((brandDB) => {
+                        return brandDB.id === brandId
+                    })
+
+                    return {
+                        brand: brand.id,
+                        name: brand.name
+                    }
+                }
+
+                res.status(200).send(products)
+            }
+
+        } catch (error) {
+            console.log(error)
+
+            if (error instanceof Error) {
+                res.status(500).send(error.message)
+            } else {
+                res.status(500).send("Erro inesperado")
+            }
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import { productRouter } from './router/productRouter'
+import { brandRouter } from './router/brandRouter'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.listen(3003, () => {
-    console.log(`Servidor rodando na porta ${3003}`)
+app.listen(3003, () => {console.log(`Servidor rodando na porta ${3003}`)
+
+app.use("/products", productRouter)
+app.use("/brands", brandRouter)
+
 })
+
